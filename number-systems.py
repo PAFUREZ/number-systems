731fd21fed78d0042cb08ac3f4161f838db38985
Shustikov Andrew <Nok2002nok2002nok@mail.ru>
2024-08-24 14:06:42 +0300

    Добавлено вычисление дробных чисел

--- number-systems.py
+++ number-systems.py
@@ -21,6 +21,40 @@ def without_hex_format(value, ns_from):
 
 
 def number_systems(value, ns_from, ns_to):
+    split = value.split('.')
+
+    if len(split) == 1:
+        return number_systems_int(value, ns_from, ns_to)
+
+    if len(split) == 2:
+        return f'{number_systems_int(split[0], ns_from, ns_to)}.{number_systems_float(
+            split[1], ns_from, ns_to
+        )}'
+
+
+def number_systems_float(value, ns_from, ns_to, count=5):
+    if ns_from == ns_to:
+        return value
+    if ns_from == 10:
+        current_count = 0
+        result = ''
+        while current_count < count:
+            result += with_hex_format(
+                int(float(f'0.{value}') * ns_to), ns_to
+            )
+            value = str(float(f'0.{value}') * ns_to).split('.')[1]
+            current_count += 1
+        return result
+    if ns_to == 10:
+        value_by_ns_to = 0
+        for i, char in enumerate(value):
+            value_by_ns_to += without_hex_format(
+                char, ns_from
+            ) * ns_from ** (-i - 1)
+        return str(value_by_ns_to).split('.')[1]
+
+
+def number_systems_int(value, ns_from, ns_to):
     if ns_from == ns_to:
         return value
     if ns_from == 10:
